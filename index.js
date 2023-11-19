65c5eca1793c674ed4f4013f412ae12e7377a8fa
antoinehage612 <hypercoffin612@gmail.com>
2023-11-19 20:32:31 +0200

    ssas

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
+const dotenv = require("dotenv");
+
+dotenv.config();
 const menuRoutes = require("./routes/menuRoutes");
 
 const app = express();
@@ -13,9 +15,10 @@ mongoose
   .then(() => console.log("Connected to DB"))
   .catch((err) => console.log(err));
 
+const PORT = process.env.PORT !== undefined ? process.env.PORT : 5000;
+
 app.use("/api/menu", menuRoutes);
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
