b37547a1dcee8d2436ee9dbad00e7de26480bd71
antoinehage612 <hypercoffin612@gmail.com>
2023-11-20 10:45:29 +0200

    neww

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ mongoose
 
 app.use("/api/menu", menuRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
