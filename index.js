b6a01a0c331a0f85ff54910cd9c695ecc53c7213
antoinehage612 <hypercoffin612@gmail.com>
2023-11-21 11:29:06 +0200

    neww

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const menuRoutes = require("./routes/menuRoutes");
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:3000/",
+    origin: "http://localhost:3000",
   })
 );
 console.log("sdssd");
